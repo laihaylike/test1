3e6be11e103a468d24f98ffe8c54184bcbf48756
laihaylike <laihaylike@gmail.com>
2020-11-25 23:22:27 +0700

    first commit

---
+++ lai1.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 2;
+
+console.log('a=10');
+console.log('b=2');
+console.log('Tong hai so la: ' + (a+b));
+console.log('Hieu hai so la: ' + (a-b));
+console.log('Tich hai so la: ' + (a*b));
+console.log('Thuong hai so la: ' + (a/b));
+console.log('a+=b la: ' + (a+=b));
+console.log('a<=b la: ' + (a<=b));
