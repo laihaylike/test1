787e3b4404a47b407ffc7ce26c93f2ea169375be
laihaylike <75034446+laihaylike@users.noreply.github.com>
2020-12-02 17:52:37 +0700

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,18 @@
+function Fibonacci(n){
+    if (n==0){
+        return 0;
+    }
+    if (n==1 || n==2){
+        return 1;
+    }
+    return Fibonacci(n-2) + Fibonacci(n-1);
+}
+
+var n = 9;
+var b = [];
+
+for (var i=1; i<=n; i++){
+    b.push(Fibonacci(i));
+}
+
+console.log(b);
